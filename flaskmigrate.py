41eae64b5dbf3448ad166581382cfe2a6f37bf01
Markland Payne <marklandpayne@gmail.com>
2024-04-24 23:03:48 -0500

    downgraded flask migrate

--- flaskmigrate.py
+++ flaskmigrate.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+# from flask import Flask
+# import sqlalchemy
+# from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 from app import app
 from app import db
